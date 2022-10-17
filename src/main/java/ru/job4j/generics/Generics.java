59a924e073d8959848485d77fb8c79f717bcd4a2
Ptz_89 <ptz_89@mail.ru>
2022-10-17 11:36:40 +0300

    0. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -0,0 +1,61 @@
+package ru.job4j.generics;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Generics {
+    public static void main(String[] args) {
+        Generics gen = new Generics();
+        List<Animal> first = new ArrayList<>();
+        List<Predator> second = new ArrayList<>();
+        List<Tiger> third = new ArrayList<>();
+        first.add(new Animal());
+        second.add(new Predator());
+        third.add(new Tiger());
+
+        gen.printObject(first);
+        gen.printObject(second);
+        gen.printObject(third);
+        System.out.println();
+
+        /**
+         * The method is not applicable to the first because of the boundedwildcard (Iterator<? extends Predator> list)
+         * Predator extends Animal
+         * gen.printBoundedWildCard(first);
+         */
+        gen.printBoundedWildCard(second);
+        gen.printBoundedWildCard(third);
+        System.out.println();
+
+        /**
+         * The method is not applicable to the third because
+         of the lowerboundedwildcard (Iterator<? super Predator> list)
+         * Predator super Animal
+         * gen.printLowerBoundedWildCard(third);
+         */
+        gen.printLowerBoundedWildCard(first);
+        gen.printLowerBoundedWildCard(second);
+    }
+
+    public void printObject(List<?> list) {
+        for (Iterator<?> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    public void printBoundedWildCard(List<? extends Predator> list) {
+        for (Iterator<? extends Animal> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    public void printLowerBoundedWildCard(List<? super Predator> list) {
+        for (Iterator<? super Tiger> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+}
