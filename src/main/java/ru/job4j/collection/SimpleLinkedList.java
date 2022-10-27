412f4f77c0868ccb6f8f386ecb9fdb3249f69c3d
Ptz_89 <ptz_89@mail.ru>
2022-10-27 13:44:53 +0300

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,74 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleLinkedList<E> implements LinkedList<E> {
+        private int size;
+        private int modCount;
+        private Node<E> first;
+        private Node<E> last;
+
+        @Override
+        public void add(E value) {
+                final Node<E> l = last;
+                final Node<E> newNode = new Node<>(l, value, null);
+                last = newNode;
+                if (l == null) {
+                        first = newNode;
+                } else {
+                        l.next = newNode;
+                }
+                size++;
+                modCount++;
+        }
+
+        @Override
+        public E get(int index) {
+                Objects.checkIndex(index, size);
+                int currentIndex = 0;
+                Node<E> rsl = first;
+                while (currentIndex != index) {
+                        rsl = rsl.next;
+                        currentIndex++;
+                }
+                return rsl.item;
+        }
+
+        @Override
+        public Iterator<E> iterator() {
+                return new Iterator<E>() {
+                        private int expectedModCount = modCount;
+                        private Node<E> value = first;
+
+                        @Override
+                        public boolean hasNext() {
+                                if (modCount != expectedModCount) {
+                                        throw new ConcurrentModificationException();
+                                }
+                                return value != null;
+                        }
+
+                        @Override
+                        public E next() {
+                                if (!hasNext()) {
+                                        throw new NoSuchElementException();
+                                }
+                                E rsl = value.item;
+                                value = value.next;
+                                return rsl;
+                        }
+                };
+        }
+
+        private static class Node<E> {
+                E item;
+                Node<E> next;
+                Node<E> prev;
+
+                Node(Node<E> prev, E element, Node<E> next) {
+                        this.item = element;
+                        this.next = next;
+                        this.prev = prev;
+                }
+        }
+}
