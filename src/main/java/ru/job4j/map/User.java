36672bc91f4746bac9a09fa00331f6716e2f0d83
Ptz_89 <ptz_89@mail.ru>
2022-11-07 17:42:40 +0300

    3. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,9 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class User {
     private String name;
@@ -14,6 +11,16 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 
     public static void main(String[] args) {
