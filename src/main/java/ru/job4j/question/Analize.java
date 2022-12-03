8c9195513181b48325a1beaba4a3c1e4ea9c94cf
Ptz_89 <ptz_89@mail.ru>
2022-12-03 16:42:56 +0300

    2. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,24 @@
+package ru.job4j.question;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        Info rsl = new Info();
+
+        Map<Object, Object> currentMap = current.stream().collect(Collectors.toMap(User::getId, User::getName));
+        for (User prevUser : previous) {
+            Object currUser = currentMap.get(prevUser.getId());
+            if (currUser == null) {
+                rsl.increaseDeleted();
+            } else if (!currUser.equals(prevUser.getName())) {
+                rsl.increaseChanged();
+            }
+        }
+        rsl.setAdded(currentMap.size() - previous.size() + rsl.getDeleted());
+        return rsl;
+    }
+}
