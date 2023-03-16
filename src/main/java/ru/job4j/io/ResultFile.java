42d997499f22cb0a045fa681ef31d585cc3e96e4
Ptz_89 <ptz_89@mail.ru>
2023-03-16 10:49:22 +0300

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (final FileOutputStream out = new FileOutputStream("data/multiplication table.txt")) {
+            for (int i = 1; i < 11; i++) {
+                for (int j = 1; j < 11; j++) {
+                    String str = Integer.toString(i * j);
+                    out.write(str.getBytes());
+                    out.write(" ".getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
