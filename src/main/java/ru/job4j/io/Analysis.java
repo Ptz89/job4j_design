8d7c894eed8cd195debf4e0f48654f1ebfd558fd
Ptz_89 <ptz_89@mail.ru>
2023-03-24 15:40:04 +0300

    7. Анализ доступности сервера [#859 #403778]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -3,6 +3,7 @@ package ru.job4j.io;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringJoiner;
 
 public class Analysis {
     private List<String> list = new ArrayList<>();
@@ -15,26 +16,24 @@ public class Analysis {
             String line;
             while ((line = in.readLine()) != null) {
 
-                if (list.size() == 0 && (line.startsWith("400") || line.startsWith("500"))) {
+                if ((list.size() == 0 && (line.startsWith("400") || line.startsWith("500")))
+                || (list.size() != 0 && (line.startsWith("200") || line.startsWith("300")))) {
                     String[] lines = line.split(" ");
                     list.add(lines[1]);
                 }
-                if (list.size() != 0 && (line.startsWith("200") || line.startsWith("300"))) {
-                    String[] lines = line.split(" ");
-                    list.add(lines[1]);
-                }
+
                 if (list.size() == 2) {
-                    String str = list.get(0) + ";" + list.get(1) + ";";
+                    StringBuilder str = new StringBuilder();
+                    list.forEach(s -> str.append(s).append(";"));
                     out.println(str);
                     list.clear();
                 }
             }
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public static void main(String[] args) {
         Analysis analysis = new Analysis();
         analysis.unavailable("data/server.log", "data/target.csv");
