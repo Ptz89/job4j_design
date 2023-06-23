79e4eb4ed224b4ecc33c26dd54e892eaa7f69e84
Ptz_89 <ptz_89@mail.ru>
2023-06-23 11:44:42 +0300

    10. Сканирование файловой системы [#106929 #424740]

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -12,8 +12,8 @@ import java.util.function.Predicate;
 import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class SearchFiles implements FileVisitor<Path> {
-    private List<Path> paths = new ArrayList<>();
-    private Predicate<Path> condition;
+    private final List<Path> paths = new ArrayList<>();
+    private final Predicate<Path> condition;
 
     public SearchFiles(Predicate<Path> condition) {
         this.condition = condition;
